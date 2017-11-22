ab3ae5073c0133b74ce38b436be758a1e97d2827
957685825 <957685825@qq.com>
2017-11-22 15:47:51 +0800

    s

--- static/config/config.js
+++ static/config/config.js
@@ -2,8 +2,8 @@
 //const HOST_CONFIG = 'http://m.builder.artup.com/'
 
 //测试环境
-//const HOST_CONFIG = 'http://haiguan.artup.com/'
-const HOST_CONFIG = 'http://192.168.127.51/'
+const HOST_CONFIG = 'http://haiguan.artup.com/'
+//const HOST_CONFIG = 'http://192.168.127.51/'
 
 //图片服务器（静态资源）服务器地址 -===>>>> 正式环境
 //const  HOST_STATIC_SERVER_HOST = 'http://img.artup.com/'
